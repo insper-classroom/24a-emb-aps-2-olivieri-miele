c9981dcc5fe443458ed708c93406610b8ff31996
arthurolivieri <arthuro@al.insper.edu.br>
2024-04-17 12:20:23 -0300

    mouse funcionando (faltam ajustes de sensibilidade)

--- main/main.c
+++ main/main.c
@@ -13,6 +13,8 @@
 
 #include "hc06.h"
 
+#include "hardware/adc.h"
+
 const int BTN_PIN_1 = 21;
 const int BTN_PIN_2 = 20;
 const int BTN_PIN_3 = 19;
@@ -20,7 +22,16 @@ const int BTN_PIN_4 = 18;
 const int BTN_PIN_5 = 17;
 const int BTN_PIN_6 = 16;
 
+const int PIN_X = 26;
+const int PIN_Y = 27;
+
 QueueHandle_t xQueueBtn;
+QueueSetHandle_t xQueueAdc;
+
+typedef struct adc {
+    int axis;
+    int val;
+} adc_t;
 
 void gpio_callback(uint gpio, uint32_t events) {
     int btn;
@@ -107,7 +118,136 @@ void btns_task(void *p) {
     int btn;
     while (1) {
         if (xQueueReceive(xQueueBtn, &btn, portMAX_DELAY)){
-            printf("Botão %d apertado!\n", btn);
+            //printf("Botão %d apertado!\n", btn);
+        }
+    }
+}
+
+int transform_to_centered_scale(uint16_t adc_value) {
+    // Transforma de 0-4095 para -2047 a +2047
+    return adc_value - 2048;
+}
+
+int reduce_resolution(int centered_value) {
+    // Mapeia de -2047 a +2047 para -255 a +255
+    // Isto pode ser feito dividindo o valor centralizado por 8, assumindo que a relação de escala é linear
+    return centered_value / 8;
+}
+
+int apply_dead_zone(int reduced_value) {
+    // Se o valor estiver dentro de -30 a +30, defina como zero
+    if (reduced_value >= -30 && reduced_value <= 30) {
+        return 0;
+    }
+    return reduced_value;
+}
+
+void x_task(void  *p) {
+    adc_init();
+    adc_gpio_init(PIN_X);
+
+
+    const int windowSize = 5; // Tamanho da janela para a média móvel
+    float window[5] = {0,0,0,0,0}; // Inicializa a janela com 0
+    float windowSum = 0; // Soma dos valores na janela
+    int dataIndex = 0; // Índice atual na janela
+
+
+    while (1) {
+        adc_select_input(0); // Select ADC input 0 (GPIO26)
+        uint16_t result;
+        result = adc_read();
+
+        // Aplica as transformações de escala
+        int centered_value = transform_to_centered_scale(result);
+        int reduced_value = reduce_resolution(centered_value);
+        int dead_zone_value = apply_dead_zone(reduced_value);
+
+        // Subtrai o valor antigo e adiciona o novo valor à soma da janela
+        windowSum -= window[dataIndex];
+        window[dataIndex] = dead_zone_value;
+        windowSum += dead_zone_value;
+
+        // Calcula a média móvel
+        float movingAverage = windowSum / windowSize;
+
+        // Imprime a tensão média
+        //printf("X: %f V\n", movingAverage);
+
+        adc_t x;
+        x.axis = 0;
+        x.val = movingAverage;
+
+        xQueueSend(xQueueAdc, &x, 0);
+
+        // Avança o índice da janela circularmente
+        dataIndex = (dataIndex + 1) % windowSize;
+
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
+void y_task(void *p) {
+    adc_init();
+    adc_gpio_init(PIN_Y);
+
+
+    const int windowSize = 5; // Tamanho da janela para a média móvel
+    float window[5] = {0,0,0,0,0}; // Inicializa a janela com 0
+    float windowSum = 0; // Soma dos valores na janela
+    int dataIndex = 0; // Índice atual na janela
+
+
+    while (1) {
+        adc_select_input(1); // Select ADC input 0 (GPIO26)
+        uint16_t result;
+        result = adc_read();
+
+        // Aplica as transformações de escala
+        int centered_value = transform_to_centered_scale(result);
+        int reduced_value = reduce_resolution(centered_value);
+        int dead_zone_value = apply_dead_zone(reduced_value);
+
+        // Subtrai o valor antigo e adiciona o novo valor à soma da janela
+        windowSum -= window[dataIndex];
+        window[dataIndex] = dead_zone_value;
+        windowSum += dead_zone_value;
+
+        // Calcula a média móvel
+        float movingAverage = windowSum / windowSize;
+
+        // Imprime a tensão média
+        //printf("Y: %f V\n", movingAverage);
+
+        adc_t y;
+        y.axis = 1;
+        y.val = movingAverage;
+
+        xQueueSend(xQueueAdc, &y, 0);
+
+        // Avança o índice da janela circularmente
+        dataIndex = (dataIndex + 1) % windowSize;
+
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
+void write_package(adc_t data){
+    int val = data.val;
+    int msb = val >> 8;
+    int lsb = val & 0xFF;
+    uart_putc_raw(uart0, data.axis);
+    uart_putc_raw(uart0, lsb);
+    uart_putc_raw(uart0, msb);
+    uart_putc_raw(uart0, -1);
+}
+
+void uart_task(void *p) {
+    adc_t data;
+
+    while (1) {
+        if (xQueueReceive(xQueueAdc, &data, portMAX_DELAY)){
+            write_package(data);
         }
     }
 }
@@ -115,9 +255,14 @@ void btns_task(void *p) {
 int main() {
     stdio_init_all();
 
-    printf("Start bluetooth task\n");
+    //printf("Start bluetooth task\n");
 
     xQueueBtn = xQueueCreate(32, sizeof(int));
+    xQueueAdc = xQueueCreate(32, sizeof(adc_t));
+
+    xTaskCreate(x_task, "x_task", 4096, NULL, 1, NULL);
+    xTaskCreate(y_task, "y_task", 4096, NULL, 1, NULL);
+    xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
 
     //xTaskCreate(hc06_task, "UART_Task 1", 4096, NULL, 1, NULL);
     xTaskCreate(btns_task, "btns_task", 4095, NULL, 1, NULL);
